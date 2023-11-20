c05e7b793bbfdcee43b5d8dd68c2d46f712552fd
gbar-dev <baroin.guillaume.pro@gmail.com>
2023-11-20 15:49:36 +0100

    main docstring

--- src/main.py
+++ src/main.py
@@ -1,19 +1,46 @@
 from data.make_dataset import YahooFinanceScraper
-from data.clean_dataset import pretraitement
+from data.clean_dataset import Preprocessing
 from models.linearmodel import LinearModels
 import os
 import pandas as pd
 
 def main():
+    """
+    The main function of the program.
+
+    This function checks if a CSV file containing stock market data exists.
+    If the file does not exist, it uses the YahooFinanceScraper to fetch
+    stock market data, processes the data, and builds linear models using
+    scikit-learn and statsmodels. If the file exists, it reads the data from
+    the CSV file and performs the same processing and modeling steps.
+
+    The program aims to predict asset variations based on historical data.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    # if bourse.csv doesn't exist, run crawler
     if not os.path.exists("src/data/database/bourse.csv"):
+        #crawler yahoo finance
         scraper = YahooFinanceScraper(0)
+        #scrap abbreviation
         list_action = scraper.scraper_action()
-        print(list_action)
-        scraper.scraper_asset(list_action)
-    else : 
-        # assets = pd.read_csv("C:/Users/Guillaume Baroin/Documents/Programs/asset_prediction/data/processed/action.csv")
-        action_asset,variation = pretraitement(chemin="src/data/database/bourse.csv")
+        #scrap assets
+        action_asset,variation = scraper.scraper_asset(list_action)
+        #instance linearmodels
         model = LinearModels(action_asset,variation)
+        #linear models
+        scikit_model = model.scikit_linear_model()
+        sm_model = model.sm_linear_model()
+    # else, import csv 
+    else: 
+        action_asset,variation = Preprocessing(chemin="src/data/database/bourse.csv")
+        #instance linearmodels
+        model = LinearModels(action_asset,variation)
+        #linear models
         scikit_model = model.scikit_linear_model()
         sm_model = model.sm_linear_model()
 
